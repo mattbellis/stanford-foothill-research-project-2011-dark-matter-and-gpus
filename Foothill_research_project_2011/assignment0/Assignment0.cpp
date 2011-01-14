9db054c5dec23a6970cd1a863ed8a60e28e3896f
matthew.bellis <matthew.bellis@89b7137e-46dd-d1ea-4046-6c371b9e098b>
2011-01-14 16:24:37 +0000

    Made some minor changes

--- Foothill_research_project_2011/assignment0/Assignment0.cpp
+++ Foothill_research_project_2011/assignment0/Assignment0.cpp
@@ -3,34 +3,35 @@
 
 using namespace std;
 
-double const Grav_const = 6.6742 * pow(10.0, -11);
+//double const GRAV_CONST = 6.6742 * pow(10.0, -11);
+double const GRAV_CONST = 6.6742e-11;
 
 long double GravForce(long double mass1, long double mass2, long double dist);
 double Acceleration(long double mass, long double force);
 
 int main()
 {
-   long double mass_E, mass_M, Dist, Force;
-   double Accel_E, Accel_M; 
+   long double mass_E, mass_M, dist, force;
+   double accel_E, accel_M; 
 
    
    mass_E = 5.974 * pow(10.0, 24);
    mass_M = 7.349 * pow(10.0, 22);
-   Dist = 3.84403 * pow(10.0, 8);
+   dist = 3.84403 * pow(10.0, 8);
 
-   Force = GravForce(mass_E, mass_M, Dist);
-   Accel_E = Acceleration(mass_E, Force);
-   Accel_M = Acceleration(mass_M, Force);
+   force = GravForce(mass_E, mass_M, dist);
+   accel_E = Acceleration(mass_E, force);
+   accel_M = Acceleration(mass_M, force);
 
    cout.setf(ios::fixed);
    cout.precision(5);
 
-   cout << "The Gravidational Force between Earth and Moon is:\n"
-        << Force / pow(10.0, 20) << " * 10^20 Newton" << endl <<endl;
+   cout << "The Gravitational Force between Earth and Moon is:\n"
+        << force / pow(10.0, 20) << " * 10^20 Newton" << endl <<endl;
 
-   cout << "The accelereation of Earth is: " << Accel_E << " m/s/s\n\n";
+   cout << "The acceleration of Earth is: " << accel_E << " m/s/s\n\n";
 
-   cout << "The acceleration of Moon is: " << Accel_M << " m/s/s\n\n";
+   cout << "The acceleration of Moon is: " << accel_M << " m/s/s\n\n";
 
    return 0;
 }
@@ -39,7 +40,7 @@ long double GravForce(long double mass1, long double mass2, long double dist)
 {
    double gForce;
 
-   gForce = (Grav_const * mass1 * mass2) / (dist * dist);
+   gForce = (GRAV_CONST * mass1 * mass2) / (dist * dist);
 
    return gForce;
 }
