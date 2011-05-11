bcce565f35a7a07ef1133f97a18662aa45c6ec91
hasyeprem <hasyeprem@89b7137e-46dd-d1ea-4046-6c371b9e098b>
2011-05-11 19:41:16 +0000

    Cuda directory

---
+++ trunk/Foothill_research_project_2011/CUDA_assignments/CUDA_copy_coordinates.cu
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<cstdlib>
+#include<fstream>
+
+__global__ voidcoppy_coordinates(float *dev_x, int *dev_y, int *dev_z)
+{
+   
+}
+
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+    float *dev_pos_x, *dev_pos_y, *dev_pos_z;
+
+    int NUM_PARTICLES;
+
+    if (argc < 3)
+    {
+        cerr << endl;
+        cerr << "Must pass in cluster_data file  on command line!" << endl;
+        cerr << "Usage: " << argv[0] << " <cluster_data file> <distances file> " << endl;
+        cerr << endl;
+        exit(1);
+    }
+
+    ifstream infile(argv[1]);
+
+    //////////////////////////////////////////////////////////////////////
+    // Read in the cluster_data file
+    ////////////////////////////////////////////////////////////////////////////
+    infile >> NUM_PARTICLES;
+
+    string axis_titles;
+    for(int m=0; m <3; m++)
+       infile >> axis_titles >> axis_titles;
+
+    float pos[NUM_PARTICLES][3];
+
+    string dummy;
+
+    if(infile.good())
+    {
+        for(int i=0; i<NUM_PARTICLES; i++)
+        {
+
+            infile >> pos[i][0] >> dummy;
+            infile >> pos[i][1] >> dummy;
+            infile >> pos[i][2] >> dummy;
+        }
+
+    }
+    else
+     {
+        cerr << "Couldn't open the file for input." << endl;
+        exit(1);
+    }
+    ////////////////////////////////////////////////////////////////////////////
+    
+    int size = NUM_PARTICLES * sizeof(float);    
+
+    cudaMalloc((void **) &dev_pos_x, size );
+    cudaMalloc((void **) &dev_pos_y, size );
+    cudaMalloc((void **) &dev_pos_z, size );
+
+    cudaMemcpy(dev_pos_x, pos[0], size, cudaMemcpyHostToDevice );
+   
+
+    cudaFree(dev_pos_x);
+    cudaFree(dev_pos_y);  
+    cudaFree(dev_pos_z);
+
+   return 0;
+}  
