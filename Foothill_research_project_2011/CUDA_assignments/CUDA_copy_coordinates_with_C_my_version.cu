c71f21a06ff3b74429b133fde443a61303f776f1
hasyeprem <hasyeprem@89b7137e-46dd-d1ea-4046-6c371b9e098b>
2011-06-06 19:33:43 +0000

    commented out

--- Foothill_research_project_2011/CUDA_assignments/CUDA_copy_coordinates_with_C_my_version.cu
+++ Foothill_research_project_2011/CUDA_assignments/CUDA_copy_coordinates_with_C_my_version.cu
@@ -113,7 +113,7 @@ int main(int argc, char **argv)
     cudaMemcpy(dev_pos_z, pos_z, size, cudaMemcpyHostToDevice );
 
 
-    distance<<<16, 512 >>>(dev_pos_x, dev_pos_y, dev_pos_z, NUM_PARTICLES);//, dev_dist);
+    distance<<<128 , 512 >>>(dev_pos_x, dev_pos_y, dev_pos_z, NUM_PARTICLES);//, dev_dist);
 
     //cudaMemset(dev_dist,1.0,size*size);
    // cudaMemcpy(h_dist, dev_dist, size * size, cudaMemcpyDeviceToHost );
