c17fb371143307710cb012154d6690dfd976dc74
hasyeprem <hasyeprem@89b7137e-46dd-d1ea-4046-6c371b9e098b>
2011-06-28 18:37:38 +0000

    python script for RA , Decl and Redshift

--- trunk/Foothill_research_project_2011/Real_data/parse_RA_Decl_and_RedShidt.py
+++ trunk/Foothill_research_project_2011/Real_data/parse_RA_Decl_and_RedShidt.py
@@ -4,38 +4,34 @@ import sys
 from numpy import*
 from math import pi
 infilename = sys.argv[1]
-num_lines=40012
+num_lines=40000
 Radian = pi / 180
 infile = open(infilename,'r')
 
 ###outfilename = "parsed_file_%s_line%d.csv" % (infilename.split('.')[0], line_to_parse)
-outfilename = "parsed_file_A_Decl_Red_shift.csv"
+outfilename = sys.argv[2] 
 outfile = open(outfilename,'w+')
 
-output = ""
 #output = "RA, Declination , Red_shift\n"
 #outfile.write(output)
 
+output = "%i\n" % (num_lines)
+outfile.write(output)
+output = ""
+
 for i, line in enumerate(infile):
-    if i > 11 and i < num_lines:
+    if i > 11 and i < num_lines+12:
        line_num = line.split()      
-    
-       for j, col in enumerate(line_num):
-           if j == 1 or j == 2: 
-              RA_Decl = float(col) * Radian
-              output += "%.4f , " % (RA_Decl)
-           if j == 24:
-              output += " %s\n" % (col)
-        #  print col
-        #  if j == 1:
-        #     RA = float(col) * Radian
-        #     output += "%.4f " % (RA)
-        #  if j == 2:
-        #     decl = float(col) 
-        #     output += ", %.4f" % (col)
-           outfile.write(output)
+       
+       for j in range(1,3):     
+           RA_Decl = float(line_num[j]) * Radian
+           output += "%.4f , " % (RA_Decl)
+      
+#       output += " %s " % (line_num[24])      
+       output += "\n"   
+       outfile.write(output)
   
-           output = ""
+       output = ""
 
 outfile.close()
     
